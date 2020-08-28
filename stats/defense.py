9cfac7dae2a23475039e5f5340a279e4b9fc795c
serugeneris <tute_2193@hotmail.com>
2020-08-28 10:59:23 -0300

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -10,13 +10,12 @@ pa = plays.loc[plays['player'].shift() != plays['player'], ['year','game_id', 'i
 pa = pa.groupby(['year','game_id','team']).size().reset_index(name='PA')
 
 events = events.set_index(['year','game','team','event_type'])
-
 events = events.unstack().fillna(0).reset_index()
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
 events = events.rename_axis(None, axis='columns')
-events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
 
+events_plus_pa = pd.merge(events, pa, how='outer', left_on=['year', 'game_id', 'team'], right_on=['year', 'game_id', 'team'])
 defense = pd.merge(events_plus_pa, info)
 defense.loc[:,'DER'] = 1 - ((defense['H'] + defense['ROE']) / (defense['PA'] - defense['BB'] - defense['SO'] - defense['HBP'] - defense['HR']))
 defense.loc[:,'year'] = pd.to_numeric(defense['year'])
